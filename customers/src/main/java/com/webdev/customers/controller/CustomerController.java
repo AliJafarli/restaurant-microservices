cf0723169177651cad22ed681ef2af4c98704977
AliJafarli <somagni4icent@gmail.com>
2025-11-05 20:45:53 +0400

    getAllCustomers()

--- customers/src/main/java/com/webdev/customers/controller/CustomerController.java
+++ customers/src/main/java/com/webdev/customers/controller/CustomerController.java
@@ -4,9 +4,9 @@ import com.webdev.customers.entity.Customer;
 import com.webdev.customers.service.ICustomerService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @RestController
@@ -16,9 +16,15 @@ public class CustomerController {
 
     private final ICustomerService customerService;
 
-public ResponseEntity<String> createCustomer(@RequestBody Customer customer) {
-    customerService.createCustomer(customer);
-    return ResponseEntity.ok("Customer created and added to the database!");
-}
+    @PostMapping
+    public ResponseEntity<String> createCustomer(@RequestBody Customer customer) {
+        customerService.createCustomer(customer);
+        return ResponseEntity.ok("Customer created and added to the database!");
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Customer>> getAllCustomers() {
+        return ResponseEntity.ok(customerService.getAllCustomers());
+    }
 
 }
